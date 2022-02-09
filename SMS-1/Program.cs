1b9e3dc30266a8907513964ea941d307c684175c
Dang Nguyen <dangnt@sgu.edu.vn>
2022-02-09 11:33:35 +0700

    Add project files.

---
+++ SMS-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            UndergraduateStudent[]  uStudents = new UndergraduateStudent[10];
+
+            uStudents[0] = new UndergraduateStudent("GCS2022-001", "Nguyen An", "CS");
+            uStudents[1] = new UndergraduateStudent();
+
+            uStudents[1].ID = "GCS2022-002";
+            uStudents[1].Name = "Tran Binh";
+            uStudents[1].Major = "CS";
+
+            uStudents[0].printUndergraduateStudent();
+            uStudents[1].printUndergraduateStudent();
+        }
+    }
+}
---
+++ SMS-1/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMS_1
+{
+    internal class Student
+    {
+        protected string name;
+        protected string id;
+
+        public Student() { }
+
+        public Student(string id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public string Name 
+        { 
+            get { return name; } 
+            set { name = value; }
+        }
+
+        public string ID 
+        {
+            get { return id; } 
+            set { id = value; }
+        }
+
+        public void printStudent()
+        {
+            Console.WriteLine($"ID: {id}    Name: {name}");
+        }
+    }
+}
